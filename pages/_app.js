133fd5aadf077d7637f44c6da61df81533cd2066
Chris Watt-Pringle <53357288+chichiwattyp2@users.noreply.github.com>
2025-09-29 23:56:16 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,12 @@
 import Script from "next/script";
-import "../styles/globals.css"; // keep this if you already have global styles
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* Load AR.js Studio backend script before any page code runs */}
       <Script
         src="https://raw.githack.com/AR-js-org/studio-backend/master/dist/arjs-studio-backend.min.js"
         strategy="beforeInteractive"
       />
-
       <Component {...pageProps} />
     </>
   );
