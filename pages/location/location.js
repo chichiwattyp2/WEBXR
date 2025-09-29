fb3046fccbb3093387f40e242cbc7079a2d6e94e
Chris Watt-Pringle <53357288+chichiwattyp2@users.noreply.github.com>
2025-09-30 00:01:43 +0200

    Delete pages directory

--- pages/location/location.js
+++
@@ -1,7 +0,0 @@
-import dynamic from "next/dynamic";
-
-const LocationPage = dynamic(() => import("../../components/LocationPage"), {
-  ssr: false, // 🚀 disable SSR
-});
-
-export default LocationPage;
