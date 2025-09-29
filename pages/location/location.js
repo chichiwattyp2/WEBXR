9f47f0a2c88a5a733921de12b0f5dcf5ed31b862
Chris Watt-Pringle <53357288+chichiwattyp2@users.noreply.github.com>
2025-09-29 23:59:19 +0200

    Update location.js

--- pages/location/location.js
+++ pages/location/location.js
@@ -1,16 +1,7 @@
-// pages/location/location.js
-const { Package } = ARjsStudioBackend;
-// create the package
-const pkg = new Package({
-    arType: 'location',
-    assetType: window.assetType, // image/audio/video/3d
-    assetFile: window.assetFile,
-    assetParam: window.assetParam
+import dynamic from "next/dynamic";
+
+const LocationPage = dynamic(() => import("../../components/LocationPage"), {
+  ssr: false, // 🚀 disable SSR
 });
 
-pkg.serve({ packageType: 'zip' }).then((base64) => {
-    const link = document.createElement('a');
-    link.href = `data:application/zip;base64,${base64}`;
-    link.download = 'ar.zip';
-    link.click();
-});
+export default LocationPage;
