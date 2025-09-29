342898f87feaf4b38e188f826c9dc58896f19586
Chris Watt-Pringle <53357288+chichiwattyp2@users.noreply.github.com>
2025-09-29 23:34:27 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // ✅ Ignore ESLint errors during production builds
+    ignoreDuringBuilds: true,
+  },
+  /* other config options here */
 };
 
 export default nextConfig;
